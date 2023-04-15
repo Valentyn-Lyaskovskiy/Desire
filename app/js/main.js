d8fa5d66a6abe9fcb0e1e4dff56388e86a233cba
Valentyn <riodemouro2017@gmail.com>
2023-04-15 23:46:58 +0300

    ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,4 @@
 $(function () {
-
   $(".header__btn").on("click", function () {
     $(".right-side-menu").removeClass("right-side-menu_close");
   });
@@ -11,19 +10,26 @@ $(function () {
     dots: true,
     arrows: false,
     fade: true,
-    autoplay: true
+    autoplay: true,
   });
 
   $(".contact__slider").slick({
     dots: true,
     arrows: false,
     slidesToShow: 10,
-    slidesToScroll: 10
+    slidesToScroll: 10,
+  });
+
+  $(".blog__article-slider-box").slick({
+    prevArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrow-left"><img src="./images/slider-left-arrow.svg" alt="arrow icon"></button>',
+    nextArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrow-right"><img src="./images/slider-arrow-right.png" alt="arrow icon"></button>',
   });
 });
 
-var mixer = mixitup('.gallery__inner', {
+var mixer = mixitup(".gallery__inner", {
   load: {
-    filter: '.office'
-  }
+    filter: ".office",
+  },
 });
